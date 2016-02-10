27818181a93b91cbdf65ce9394a125570f507903
Tyler <tjneisi@gmail.com>
2016-02-09 18:25:19 -0800

    feat: Make info windows more beautiful

--- src/js/app.js
+++ src/js/app.js
@@ -5,10 +5,22 @@ var MAP_CENTER = {lat:34.027, lng: -118.401}
 
 
 /* General Utility Functions */
+
+// Return true if the first string is a substring of the second string.
 var isSubstring = function(substring, string) {
   return string.toLowerCase().indexOf(substring.toLowerCase()) > -1;
 }
 
+// Return true if the user is using a mobile device.
+// Code found at: http://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+var mobileCheck = function() {
+  var check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+}
+
+// Determine if the user is on a mobile device
+var userOnMobileDevice = mobileCheck();
 
 /* Raw data for the places that display on the map by default */
 var placesData = [
@@ -80,13 +92,13 @@ var Place = function(placeData, map) {
 };
 
 
-
 /* Info Window Helper Functions */
 
 // Make initial html for the given Place's info window.
 var makeInfoWindowHTML = function(place) {
-  var html = '<div class="info-window"><h2 class="iw-header">' + place.name +
-    '</h2><div class="yelp-info hidden" id="yelp-' + place.yelpID + '"></div></div>'
+  var html = '<div class="iw-container"><div class="iw-header"><h2 class="iw-title">' +
+    place.name + '</h2></div><div class="iw-content"><div class="yelp-info hidden" id="yelp-' +
+    place.yelpID + '"></div></div></div>'
   return html;
 };
 
@@ -118,8 +130,9 @@ var getYelpData = function(place) {
     dataType: 'jsonp',
     jsonpCallback: 'cb',
     success: function(yelpData) {
+      console.dir(yelpData);
       place.yelpDataReceived = true;
-      populateYelpInfoDiv(yelpData, place);
+      fillYelpInfoDiv(yelpData, place);
     },
     error: function(error) {
       console.log(error);
@@ -129,22 +142,54 @@ var getYelpData = function(place) {
   $.ajax(settings);
 };
 
+var yelpHTMLtemplate = `
+  <div class="snippet-and-img-div">
+    <div class="yelp-snippet-div">
+      <p class="yelp-snippet">{0}</p>
+    </div>
+    <div class="yelp-img-div">
+      <img class="yelp-img" src="{1}" />
+    </div>
+  </div>
+  <table>
+    <tbody>
+      <tr>
+        <th>Rating:</th>
+        <td><img class="yelp-rating-img" src="{2}"></td>
+      </tr>
+      <tr>
+        <th>Phone:</th>
+        <td>{3}</td>
+      </tr>
+    </tbody>
+  </table>
+`;
+
+var makeYelpHTML = function(yelpData, place) {
+  var html = yelpHTMLtemplate;
+  var args = [
+    yelpData.snippet_text,
+    yelpData.image_url,
+    yelpData.rating_img_url,
+    yelpData.display_phone
+  ];
+  for (var i = 0; i < args.length; i++) {
+    html = html.replace('{' + i + '}', args[i]);
+  }
+  return html
+};
+
 // Fill in the yelp-info div of the given place's info window with data
 // returned from a yelp business api request.
-var populateYelpInfoDiv = function(yelpData, place) {
+var fillYelpInfoDiv = function(yelpData, place) {
   var $yelpInfoDiv = $('#yelp-' + place.yelpID);
-  var $table = $('<table><tr><th>Rating:</th><td></td></tr></table>');
-  var $ratingImg = $('<img class="yelp-rating-img" src="' + yelpData.rating_img_url + '" />');
-  $table.find('td').append($ratingImg);
-  $table.append('<tr><th>Phone:</th><td>' + yelpData.display_phone + '</td></tr>');
-  var $img = $('<img class="yelp-img" src="' + yelpData.image_url + '" />');
-  $yelpInfoDiv.append($img);
-  $yelpInfoDiv.append($table);
-  $img.on('load', function() { $yelpInfoDiv.fadeIn('slow', function() {
+  var $html = $(makeYelpHTML(yelpData, place));
+  $yelpInfoDiv.append($html);
+  $html.find('.yelp-img').on('load', function() { $yelpInfoDiv.fadeIn('slow', function() {
     $yelpInfoDiv.removeClass('hidden')
     place.marker.infowindow.content = $yelpInfoDiv.parent().prop('outerHTML');
   }) });
-}
+};
 
 
 /* Map Marker Animations */ 
@@ -214,7 +259,7 @@ function AppViewModel() {
         title: place.name,
         map: self.map,
         animation: google.maps.Animation.DROP,
-        infowindow: new InfoBubble({ map: self.map, content: makeInfoWindowHTML(place) })
+        infowindow: new google.maps.InfoWindow({ content: makeInfoWindowHTML(place) })
       });
       
       // Add event listeners to markers and info windows
